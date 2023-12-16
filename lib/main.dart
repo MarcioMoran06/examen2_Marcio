0c8a3f8d2a8955edd1fa8bad409c4241eecb8137
Marcio Moran <marciomoran00@gmail.com>
2023-12-16 08:17:33 -0600

    Tercer commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_examen/providers/book_list_providers.dart';
+import 'package:flutter_examen/services/get_book_answer.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
---
+++ lib/services/get_book_answer.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_examen/providers/book_list_providers.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BookListScreen extends StatefulWidget {
+  @override
+  _BookListScreenState createState() => _BookListScreenState();
+}
+
+class _BookListScreenState extends State<BookListScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() =>
+        Provider.of<BookListProvider>(context, listen: false).fetchBooks());
+  }
+ @override
+  Widget build(BuildContext context) {
+    return Consumer<BookListProvider>(
+      builder: (context, bookListProvider, child) {
+        if (bookListProvider.books.isEmpty) {
+          return Center(child: CircularProgressIndicator());
+        }
+        return ListView.builder(
+          itemCount: bookListProvider.books.length,
+          itemBuilder: (context, index) {
+            final book = bookListProvider.books[index];
+            return ListTile(
+              title: Text(book.title),
+              subtitle: Text(book.description),
+              onTap: () {
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
